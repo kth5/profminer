0b050ffa7b25bdd7761e3eaf625b64e4c5a0b369
kth5 <alex.bldck@gmail.com>
2017-01-11 00:21:36 +0800

    abort build of nheqminer on OSX as it's not gonna work anyway

--- miners-bootstrap/build-nheqminer.sh
+++ miners-bootstrap/build-nheqminer.sh
@@ -2,6 +2,11 @@
 
 source check-env.sh
 
+if [ "${OS}" = "Darwin" ]; then
+	echo ":: OSX isn't supported."
+	exit 1
+fi
+
 # fix bug with later glibc/boost combinations than nheqminer was originally
 # intended for
 export CXXFLAGS="${CXXFLAGS} -lpthread"
