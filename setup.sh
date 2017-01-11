020ed927a3781443c2a4fde9e2db63482a20dd40
kth5 <alex.bldck@gmail.com>
2017-01-11 09:54:15 +0800

    build miners during setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+PW=$(pwd)
+cd miners-bootstrap/
+./build-cpuminer-opt.sh
+./build-nheqminer.sh
+
 wget -O .rates.json "https://www.nicehash.com/api?method=stats.global.current&location=0"
